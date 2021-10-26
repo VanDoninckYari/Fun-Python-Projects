32a704dfd047058b425de7b5bf7176272089df5a
VanDoninckYari <r0793290@student.thomasmore.be>
2021-10-26 21:55:23 +0200

    Madlibs programs

---
+++ Madlibs/madlibs_random.py
@@ -0,0 +1,54 @@
+# Mad Libs Random Story Generator
+
+from random import randint
+import copy
+
+story = (
+    "One day my {} friend and I decided to go to the {} game in {}. " +
+    "We really wanted to see the {} play the {}. " +
+    "So, we {} our {} down to the {} and bought some {}s. " +
+    "We got into the game and it was a lot of fun. " +
+    "We ate some {} {} and drank some {} {}. " +
+    "We had a great time! We plan to go ahead next year!"
+)
+
+# create a dictionary to lookup words by type
+word_dict = {
+    'adjective':['greedy','abrasive','grubby','groovy','rich','harsh','tasty','slow'], 
+    'city name':['Chicago','New York','Charlotte','Indianapolis','Louisville','Denver'], 
+    'noun':['people','map','music','dog','hamster','ball','hotdog','salad'], 
+    'action verb':['run','fall','crawl','scurry','cry','watch','swim','jump','bounce'], 
+    'sports noun':['ball','mit','puck','uniform','helmet','scoreboard','player'],
+    'place':['park','desert','forest','store','restaurant','waterfall']
+}
+    
+def get_word(type, local_dict):
+    words = local_dict[type]
+    count = len(words)-1
+    index = randint(0, count)
+    return local_dict[type].pop(index)
+
+def create_story():
+    local_dict = copy.deepcopy(word_dict)
+    return story.format(
+        get_word('adjective', local_dict),
+        get_word('sports noun', local_dict),
+        get_word('city name', local_dict),
+        get_word('noun', local_dict),
+        get_word('noun', local_dict),
+        get_word('action verb', local_dict),
+        get_word('noun', local_dict),
+        get_word('place', local_dict),
+        get_word('noun', local_dict),
+        get_word('adjective', local_dict),
+        get_word('noun', local_dict),
+        get_word('adjective', local_dict),
+        get_word('noun', local_dict)
+    )
+
+print("STORY 1: ")
+print(create_story())
+print()
+print("STORY 2: ")
+print(create_story())
+    
